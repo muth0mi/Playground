2ebd86e7fe5c9aae326ff3ab537b05940fafc6fc
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-12-24 13:50:29 +0300

    Create a dummy movie catalog endpoint

---
+++ movie-catalog-service/src/main/kotlin/io/muth0mi/moviecatalogservice/models/CatalogItem.kt
@@ -0,0 +1,7 @@
+package io.muth0mi.moviecatalogservice.models
+
+data class CatalogItem(
+    var name: String,
+    var desc: String,
+    var rating: Int,
+)
---
+++ movie-catalog-service/src/main/kotlin/io/muth0mi/moviecatalogservice/resources/MovieCatalogResource.kt
@@ -0,0 +1,19 @@
+package io.muth0mi.moviecatalogservice.resources
+
+import io.muth0mi.moviecatalogservice.models.CatalogItem
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/catalog")
+class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    fun getCatalog(@PathVariable userId: String): List<CatalogItem> {
+
+        return listOf(
+            CatalogItem("Transformers", "Test", 4)
+        )
+    }
+}
