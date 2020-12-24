cf6b8c13198ac2da8bd8ed8d65ea2a0cd5c3786f
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-12-24 16:20:51 +0300

    Introduce a Eureka server for service discovery

---
+++ discovery-server/src/test/kotlin/io/muthomi/discoveryserver/DiscoveryServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.muthomi.discoveryserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
