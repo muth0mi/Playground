769168470a55a6284d311663786abff8af86b20b
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-12-24 15:07:13 +0300

    Replace restTemplate with webclient

--- movie-catalog-service/src/main/kotlin/io/muth0mi/moviecatalogservice/MovieCatalogServiceApplication.kt
+++ movie-catalog-service/src/main/kotlin/io/muth0mi/moviecatalogservice/MovieCatalogServiceApplication.kt
@@ -2,10 +2,20 @@ package io.muth0mi.moviecatalogservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootApplication
-class MovieCatalogServiceApplication
+class MovieCatalogServiceApplication {
+
+    @Bean
+    fun getRestTemplate() = RestTemplate()
+
+    @Bean
+    fun getWebClientBuilder() = WebClient.builder()
+}
 
 fun main(args: Array<String>) {
-	runApplication<MovieCatalogServiceApplication>(*args)
+    runApplication<MovieCatalogServiceApplication>(*args)
 }
--- movie-catalog-service/src/main/kotlin/io/muth0mi/moviecatalogservice/resources/MovieCatalogResource.kt
+++ movie-catalog-service/src/main/kotlin/io/muth0mi/moviecatalogservice/resources/MovieCatalogResource.kt
@@ -3,19 +3,25 @@ package io.muth0mi.moviecatalogservice.resources
 import io.muth0mi.moviecatalogservice.models.CatalogItem
 import io.muth0mi.moviecatalogservice.models.Movie
 import io.muth0mi.moviecatalogservice.models.Rating
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 @RestController
 @RequestMapping("/catalog")
 class MovieCatalogResource {
 
+    @Autowired
+    private lateinit var restTemplate: RestTemplate
+
+    @Autowired
+    private lateinit var webClientBuilder: WebClient.Builder
+
     @RequestMapping("/{userId}")
     fun getCatalog(@PathVariable userId: String): List<CatalogItem> {
-
-        val restTemplate = RestTemplate()
 
         val ratings: List<Rating> = listOf(
             Rating("1234", 4),
@@ -24,7 +30,14 @@ class MovieCatalogResource {
 
         val catalogItems = arrayListOf<CatalogItem>()
         ratings.forEach { rating ->
-            restTemplate.getForObject("http://localhost:8081/movies/${rating.movieId}", Movie::class.java)?.let { movie ->
+            val movie = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8081/movies/${rating.movieId}")
+                .retrieve()
+                .bodyToMono(Movie::class.java)
+                .block()
+
+            movie?.let {
                 catalogItems.add(CatalogItem(movie.name, "Test", rating.rating))
             }
         }
